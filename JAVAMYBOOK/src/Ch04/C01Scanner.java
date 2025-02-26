e87e9221558256ff4595b588b3509a49f5482dfa
DESKTOP-FU2AS1G\Administrator <hallohallosh@naver.com>
2025-02-26 16:07:13 +0900

    2025-02-26

--- JAVAMYBOOK/src/Ch04/C01Scanner.java
+++ JAVAMYBOOK/src/Ch04/C01Scanner.java
@@ -6,6 +6,17 @@ public class C01Scanner {
 	public static void main(String[] args) {
 		// 자동 import : ctrl + shift + o
 		// 자동 formatting : ctrl + shift : f
+
+		// 스트림
+		// System.in : 표준입력 스트림 생성(요청)
+		// System.out : 표준출력 스트림 생성(요청)
+
+		// 코드해석
+		// new Scanner(System.in) : 표준입력 스트림을 연결한 Scanner객체생성
+		// Scanner sc : 생성된 Scanner 객체의 위치(메모리주소)를 저장한 참조변수sc
+		// Scanner sc = new Scanner(System.in);
+		// -표준입력스트림에 연결된 Scanner객체를 생성 이후
+		// 위치정보를 main Stack영역의 Scanner sc 참조변수에 초기화
 		Scanner sc = new Scanner(System.in);
 
 		System.out.printf("num1 입력 :");
