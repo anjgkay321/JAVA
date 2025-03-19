6ae615120c663043ea49d21fb7ca7d3cfd3f3bec
DESKTOP-FU2AS1G\Administrator <hallohallosh@naver.com>
2025-03-19 17:49:38 +0900

    2025-03-19

--- JAVAMYBOOK/src/Ch26/ClientUI.java
+++ JAVAMYBOOK/src/Ch26/ClientUI.java
@@ -1,193 +1,121 @@
-package Ch18;
+package Ch26;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.text.BadLocationException;
 
-class C07GUI extends JFrame implements ActionListener, KeyListener, MouseListener {
+public class ClientUI extends JFrame implements KeyListener{
+	
+	JTextArea area;
+	JTextField txt;
+	ServerSocket server;
+	Socket client;
+	String nick;
 	JButton btn1;
-	JButton btn2;
-	JButton btn3;
-	JButton input;
-	JTextField txt1;
-	JTextArea area1;
-
-	C07GUI(String title) {
-		// frame
-		super(title);
-		setBounds(100, 100, 410, 400);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// Panel
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-
-		// Component 참조변수명
-		area1 = new JTextArea(); // textarea = area1
-		area1.setBounds(10, 10, 260, 280);
-		// textarea's scroll = scroll1
-		JScrollPane scroll1 = new JScrollPane(area1);
-		scroll1.setBounds(10, 10, 250, 280);
-
-		btn1 = new JButton("파일로저장");// 파일로저장 : btn1
-		btn1.setBounds(270, 10, 110, 30);
-
-		btn2 = new JButton("1:1요청");// 1:1요청 : btn2
-		btn2.setBounds(270, 50, 110, 30);
-
-		btn3 = new JButton("대화기록보기");// 대화기록보기 : btn3
-		btn3.setBounds(270, 90, 110, 30);
-
-		JButton btn4 = new JButton("UPDATE");
-		btn3.setBounds(270, 130, 110, 30);
-
-		JButton btn5 = new JButton("DELETE");
-		btn3.setBounds(270, 170, 110, 30);
-
-		JButton btn6 = new JButton("SELECT");
-		btn3.setBounds(270, 210, 110, 30);
-
-		input = new JButton("입력"); // 입력 : input
-		input.setBounds(270, 300, 110, 30);
-
-		txt1 = new JTextField(); // textfield : txt1
-		txt1.setBounds(10, 300, 250, 30);
-
-		// event listener add
-		btn1.addActionListener(this);
-		btn2.addActionListener(this);
-		btn3.addActionListener(this);
-		input.addActionListener(this);
-		txt1.addKeyListener(this);
-		area1.addMouseListener(this);
-
-		// panel(component)
-//		panel.add(area1);
-		panel.add(scroll1);
-		panel.add(btn1);
-		panel.add(btn2);
-		panel.add(btn3);
-		panel.add(btn4);
-		panel.add(btn5);
-		panel.add(btn6);
-		panel.add(input);
-		panel.add(txt1);
-
-		// frame(panel)
-		add(panel);
-
-		// frame
-		setVisible(true);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-
-//		System.out.println("클릭!");
-		if (e.getSource() == btn1) {
-			System.out.println("파일로 저장 클릭");
-		} else if (e.getSource() == btn2) {
-			System.out.println("1:1요청 클릭");
-		} else if (e.getSource() == btn3) {
-			System.out.println("대화기록보기");
-		} else if (e.getSource() == input) {
-			System.out.println("입력");
+	
+	ClientUI(String nick){
+		super("Chat Client");
+		
+		//패널
+		JPanel pan = new JPanel();
+		pan.setLayout(null);
+		
+		//컴포넌트
+		area = new JTextArea();
+		txt = new JTextField();
+		JScrollPane scroll = new JScrollPane(area);
+		scroll.setBounds(10,10,260,300);
+		txt.setBounds(10,320,260,30);
+		
+		btn1 = new JButton("대화내용 저장");
+		
+		//이벤트 리스너 등록
+		txt.addKeyListener(this);
+			
+		//패널에 추가
+		pan.add(txt);
+		pan.add(scroll);
+		//프레임에 추가
+		add(pan);
+		
+		this.setBounds(100,100,600,400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		
+		//소켓연결작업
+		try {
+			
+			//ChatServerIP로 입력 
+			client = new Socket("192.168.16.203",5555);
+			
+			//3 수신 스레드 객체 생	성
+			ClientRecvThread recv = new ClientRecvThread(client,this);	//client : 소켓수신용
+																		//this(GUI객체) : 수신한 문자열을 Area로 append
+			Thread th1 = new Thread(recv);	 
+			//4 스레드 동작
+			th1.start();
+			
+			//5 닉네임 서버로 전달
+			this.nick = nick;
+			new DataOutputStream(client.getOutputStream()).writeUTF(nick);
+			
+			
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
+	}
+	
 
+	public static void main(String[] args) {
+		
+			Scanner sc = new Scanner(System.in);
+			System.out.print("닉네임 입력 : ");
+			String nick = sc.nextLine();
+			new ClientUI(nick);	
+			
+			
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-//		System.out.println("keyTyped..."+e.getKeyChar());
-//		System.out.println("keyTyped..."+e.getKeyCode());
-
 	}
-
 	@Override
 	public void keyPressed(KeyEvent e) {
-//		System.out.println("keyPressed..."+e.getKeyChar());	
-//		System.out.println("keyPressed..."+e.getKeyCode());
-		if (e.getSource() == txt1) {
-			if (e.getKeyCode() == 10) {
-				String message = txt1.getText();
-				System.out.println(message);
-				area1.append(message + "\n");
-				txt1.setText("");
-			}
-		}
-	}
+		if(e.getSource()==txt) {
+			if(e.getKeyCode()==10)
+			{			
+				//입력한 내용이 클라이언트로 전달되어야한다
+				try {
+					DataOutputStream Dout=new DataOutputStream(client.getOutputStream());
+					Dout.writeUTF("["+nick+"] : "+txt.getText());
+					Dout.flush();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				//Send스레드에 입력한 내용을 전달
+				area.append("["+nick+"] : "+txt.getText()+"\n");
+				txt.setText("");
 
+			}
+		}	
+	}
 	@Override
 	public void keyReleased(KeyEvent e) {
-//		System.out.println("keyReleased..."+e.getKeyChar());
-//		System.out.println("keyReleased..."+e.getKeyChar());
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		try {
-
-//			System.out.println("mouseClicked..." + e.getPoint());
-			int offset = area1.viewToModel(e.getPoint());
-//			System.out.println("mouseClicked...offset : " + offset);
-			int row = area1.getLineOfOffset(offset);
-//			System.out.println("mouseClicked...row : " + row);
-			int startOffset = area1.getLineStartOffset(row);
-			int endOffset = area1.getLineEndOffset(row);
-//			System.out.printf("%d %d\n", startOffset,endOffset);
-			String str = area1.getText(startOffset, endOffset - startOffset);
-			System.out.println(str);
-
-		} catch (BadLocationException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
-
-public class C07SwingEventMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new C07GUI("Chatting UI");
-
-	}
-
 }
